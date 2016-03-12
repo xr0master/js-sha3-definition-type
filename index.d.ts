05e974b0508c818673a1603228d3c28e29b7bfec
xr0master <xr0master@gmail.com>
2016-03-12 20:30:09 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
+// Type definitions for js-sha3 v0.5.2
+// Project: https://github.com/emn178/js-sha3
+// Definitions by: Sergey Khomushin <https://github.com/xr0master>
+// Definitions: https://github.com/xr0master/js-sha3-definition-type
 declare module "js-sha3" {
   export function sha3_512(message: string): string;
   export function sha3_384(message: string): string;
