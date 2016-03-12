a7fa9e974b64eafa9978c7d33b934805da2a6fcb
xr0master <xr0master@gmail.com>
2016-03-13 00:51:39 +0200

    Update js-sha3.d.ts

--- js-sha3.d.ts
+++ js-sha3.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/emn178/js-sha3
 // Definitions by: Sergey Khomushin <https://github.com/xr0master>
 // Definitions: https://github.com/xr0master/js-sha3-definition-type
-declare module "js-sha3" {
+declare module 'js-sha3' {
   export function sha3_512(message: string): string;
   export function sha3_384(message: string): string;
   export function sha3_256(message: string): string;
